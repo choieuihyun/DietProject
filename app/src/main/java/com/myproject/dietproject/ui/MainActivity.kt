55f47e2e2d4c19424a61a4848214dc3b3915024b
choieuihyun <amammmaa1@naver.com>
2023-05-10 21:30:18 +0900

    refactor : jetpack Navigation , BottomNavigation 코드 수정

--- app/src/main/java/com/myproject/dietproject/ui/MainActivity.kt
+++ app/src/main/java/com/myproject/dietproject/ui/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.myproject.dietproject.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.myproject.dietproject.R
@@ -31,11 +36,20 @@ class MainActivity : AppCompatActivity() {
 
         setupNavigation()
 
+//        binding.bottomNavigationView.setOnClickListener {
+//            when(it.id) {
+//                R.id.calendarFragment -> {
+//                    Navigation.findNavController(binding.root).navigate(R.id.action_homeFragment_to_calendarFragment)
+//                    Log.d("asdasdasd","asdasdasd")
+//                }
+//            }
+//        }
+
     }
 
     private fun setupNavigation() {
 
-        val host = supportFragmentManager.findFragmentById(R.id.nav_graph) as NavHostFragment? ?: return
+        val host = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
         navController = host.navController
 
         binding.bottomNavigationView.setupWithNavController(navController)
