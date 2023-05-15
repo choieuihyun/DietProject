4abc3070321019ff5adb8376c5bf469380fb0e75
choieuihyun <amammmaa1@naver.com>
2023-05-15 20:59:37 +0900

    feature_05 / API 사용중 NetWorkError 처리

---
+++ app/src/main/java/com/myproject/dietproject/domain/error/NetworkError.kt
@@ -0,0 +1,10 @@
+package com.myproject.dietproject.domain.error
+
+sealed class NetworkError {
+
+    object Unknown : NetworkError()
+    object Timeout : NetworkError()
+    object InternalServer : NetworkError()
+    class BadRequest(val code: Int) : NetworkError()
+
+}
