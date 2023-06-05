ae8dfafe9837c2021f59d7f266dee7836ada715e
choeuihyeon <amammmaa1@naver.com>
2023-06-05 20:54:01 +0900

    feature : Kcal 데이터 RecyclerView Adapter 작성, List clickEvent 구현

--- presentation/src/main/java/com/myproject/dietproject/presentation/ui/userkcal/KcalAdapter.kt
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/userkcal/KcalAdapter.kt
@@ -5,22 +5,67 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import com.myproject.dietproject.domain.model.Kcal
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.myproject.dietproject.presentation.databinding.KcalListItemBinding
+import com.myproject.dietproject.presentation.databinding.KcalListItemTypeTwoBinding
 
-class KcalAdapter : ListAdapter<Kcal, KcalViewHolder>(kcalDataDiffCallback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KcalViewHolder {
-        return KcalViewHolder(
-            KcalListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        )
+class KcalAdapter : ListAdapter<Kcal, ViewHolder>(kcalDataDiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return when(viewType) {
+            type_one -> {
+                KcalViewHolder(KcalListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            }
+            type_two -> {
+                KcalViewHolderTypeTwo(KcalListItemTypeTwoBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            }
+            else -> {
+                KcalViewHolderTypeTwo(KcalListItemTypeTwoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                )
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: KcalViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         val userKcal = currentList[position]
-        holder.bind(userKcal)
+
+        when(holder) {
+
+            is KcalViewHolder -> holder.bind(userKcal)
+
+            is KcalViewHolderTypeTwo -> holder.bind(userKcal)
+
+        }
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.let {
+                it(userKcal)
+            }
+        }
+
     }
+
+    override fun getItemViewType(position: Int): Int {
+        return if(currentList[position].mAKERNAME != "")
+            type_one
+        else
+            type_two
+    }
+
+    private var onItemClickListener: ((Kcal) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Kcal) -> Unit) {
+        onItemClickListener = listener
+    }
+
 
     companion object {
+
+        const val type_one = 1
+        const val type_two = 2
 
         private val kcalDataDiffCallback = object : DiffUtil.ItemCallback<Kcal>() {
 
