fa694629277b19704fd2b7ebd33bc50d9978009e
choeuihyeon <amammmaa1@naver.com>
2023-06-10 22:41:46 +0900

    feature : CalendarDetail 기능 구현 완료

--- presentation/src/main/java/com/myproject/dietproject/presentation/ui/calendar/CalendarDetailViewModel.kt
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/calendar/CalendarDetailViewModel.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.viewModelScope
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.myproject.dietproject.domain.model.TodayKcal
+import com.myproject.dietproject.domain.model.TodayKcalForCalendar
 import com.myproject.dietproject.domain.usecase.GetUserTodayKcalUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -22,15 +24,15 @@ class CalendarDetailViewModel @Inject constructor(
     private val _dayKcal: MutableLiveData<Int> = MutableLiveData()
     val dayKcal: LiveData<Int> = _dayKcal
 
-    private val _dayKcalList: MutableLiveData<List<Any>> = MutableLiveData()
-    val dayKcalList: LiveData<List<Any>> = _dayKcalList
+    private val _dayKcalList: MutableLiveData<ArrayList<TodayKcalForCalendar?>> = MutableLiveData()
+    val dayKcalList: LiveData<ArrayList<TodayKcalForCalendar?>> = _dayKcalList
 
     fun getCalendarDetailData(userId: String, date: String) {
 
         viewModelScope.launch(Dispatchers.IO) {
 
             var sum = 0
-            val kcalArrayList = ArrayList<Any?>()
+            val kcalArrayList = ArrayList<TodayKcalForCalendar?>()
 
             getUserTodayKcalUseCase(userId,date).addListenerForSingleValueEvent(object : ValueEventListener{
                 override fun onDataChange(snapshot: DataSnapshot) {
@@ -41,12 +43,18 @@ class CalendarDetailViewModel @Inject constructor(
                         if (dataDate == date) {
                             val kcal = data.child("kcal").value
                             sum += kcal.toString().toInt()
-
-                            kcalArrayList.add(data.value)
+                            val todayKcalForCalendar = TodayKcalForCalendar(
+                                kcal = kcal.toString().toFloat(),
+                                foodName = data.child("foodName").value.toString(),
+                                makerName = data.child("makerName").value.toString()
+                            )
+                            Log.d("calendarDetail4", todayKcalForCalendar.toString())
+                            kcalArrayList.add(todayKcalForCalendar)
                         }
 
                     }
-                    _dayKcalList.value = listOf(kcalArrayList) // 어차피 읽어오기만 하니까 읽기 전용 리스트로의 변환인 listOf
+
+                    _dayKcalList.value = kcalArrayList // 어차피 읽어오기만 하니까 읽기 전용 리스트로의 변환인 listOf
                     _dayKcal.value = sum
 
                     Log.d("calendarDetail1", _dayKcalList.value.toString())
