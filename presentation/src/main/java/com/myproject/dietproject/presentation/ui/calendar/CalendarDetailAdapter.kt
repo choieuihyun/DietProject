6ec4392ea62885fd5eb46b58003659ce07771888
choeuihyeon <amammmaa1@naver.com>
2023-06-10 22:40:58 +0900

    feature : CalendarDetail RecyclerView adapter, viewHolder 구현 완료

---
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/calendar/CalendarDetailAdapter.kt
@@ -0,0 +1,74 @@
+package com.myproject.dietproject.presentation.ui.calendar
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.myproject.dietproject.domain.model.TodayKcal
+import com.myproject.dietproject.domain.model.TodayKcalForCalendar
+import com.myproject.dietproject.presentation.databinding.CalendarDetailListItemBinding
+import com.myproject.dietproject.presentation.databinding.CalendarDetailListItemTypeTwoBinding
+
+class CalendarDetailAdapter : ListAdapter<TodayKcalForCalendar, ViewHolder>(kcalDataDiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return when(viewType) {
+
+            type_one -> {
+                CalendarDetailViewHolder(CalendarDetailListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            }
+            type_two -> {
+                CalendarDetailViewHolderTypeTwo(CalendarDetailListItemTypeTwoBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                ))
+            }
+            else -> {
+                CalendarDetailViewHolderTypeTwo(CalendarDetailListItemTypeTwoBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                ))
+            }
+
+        }
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val todayKcal = currentList[position]
+
+        when(holder) {
+
+            is CalendarDetailViewHolder -> holder.bind(todayKcal)
+
+            is CalendarDetailViewHolderTypeTwo -> holder.bind(todayKcal)
+
+        }
+
+    }
+
+
+    companion object {
+
+        const val type_one = 1
+        const val type_two = 2
+
+        private val kcalDataDiffCallback = object : DiffUtil.ItemCallback<TodayKcalForCalendar>() {
+
+            override fun areItemsTheSame(oldItem: TodayKcalForCalendar, newItem: TodayKcalForCalendar): Boolean {
+                return oldItem.hashCode() == newItem.hashCode()
+            }
+
+            override fun areContentsTheSame(oldItem: TodayKcalForCalendar, newItem: TodayKcalForCalendar): Boolean {
+                return oldItem == newItem
+            }
+
+
+        }
+
+    }
+
+
+}
