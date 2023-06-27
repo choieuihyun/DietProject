aa8af1357f077a15e02110a52ce697608e02a4cb
choeuihyeon <amammmaa1@naver.com>
2023-06-27 23:50:33 +0900

    feature : infoFragmentDetail 생성

---
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/info/InfoFragmentDetail.kt
@@ -0,0 +1,12 @@
+package com.myproject.dietproject.presentation.ui.info
+
+import com.myproject.dietproject.presentation.R
+import com.myproject.dietproject.presentation.databinding.InfoFragmentDetailBinding
+import com.myproject.dietproject.presentation.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class InfoFragmentDetail : BaseFragment<InfoFragmentDetailBinding>(R.layout.info_fragment_detail){
+
+}
