eeeb92bebab288ed4852b7a4f67ea03edcbb081f
choeuihyeon <amammmaa1@naver.com>
2023-06-27 23:50:20 +0900

    feature : infoFragment 해당 정보 관련 데이터 받아오는 코드 변경

--- presentation/src/main/java/com/myproject/dietproject/presentation/ui/info/InfoFragment.kt
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/info/InfoFragment.kt
@@ -3,11 +3,9 @@ package com.myproject.dietproject.presentation.ui.info
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
-import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -21,12 +19,12 @@ class InfoFragment : BaseFragment<InfoFragmentBinding>(R.layout.info_fragment) {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val viewModel: InfoViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         auth = Firebase.auth
-
 
     }
 
@@ -35,15 +33,41 @@ class InfoFragment : BaseFragment<InfoFragmentBinding>(R.layout.info_fragment) {
 
         Log.d("asdasdasd", auth.currentUser.toString())
 
+        binding.viewModel = viewModel
+
+        viewModel.getUserEmail(auth.currentUser!!.uid)
+        viewModel.getUserRecommendKcal(auth.currentUser!!.uid)
+        viewModel.getUserMaxKcal(auth.currentUser!!.uid)
+        viewModel.getMostNumerousDate(auth.currentUser!!.uid)
+
+//        viewModel.email.observe(viewLifecycleOwner) {
+//            binding.profileEmail.text = it
+//        }
 
 
-        googleLogout()
+        // googleLogout()
 
+        binding.settingButton.setOnClickListener {
+
+            Navigation.findNavController(binding.root).navigate(R.id.action_InfoFragment_to_infoFragmentDetail)
+
+        }
 
 
     }
 
-    private fun googleLogout() {
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
+
+
+
+
+}
+
+/*    private fun googleLogout() {
 
         val gso: GoogleSignInOptions =
             GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -62,7 +86,5 @@ class InfoFragment : BaseFragment<InfoFragmentBinding>(R.layout.info_fragment) {
 
         }
 
-    }
+    }*/
 
-
-}
