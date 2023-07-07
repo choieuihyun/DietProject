678deae1d31138c2806640d92dc169488a61b18e
choeuihyeon <amammmaa1@naver.com>
2023-07-07 23:40:59 +0900

    feature : Calendar 오늘 날짜 강조 구현

--- presentation/src/main/java/com/myproject/dietproject/presentation/ui/calendar/CalendarFragment.kt
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/calendar/CalendarFragment.kt
@@ -8,8 +8,11 @@ import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.core.view.doOnPreDraw
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -25,6 +28,7 @@ import com.myproject.dietproject.presentation.databinding.CalendarDayLayoutBindi
 import com.myproject.dietproject.presentation.databinding.CalendarFragmentBinding
 import com.myproject.dietproject.presentation.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.DayOfWeek
 
 import java.time.LocalDate
 import java.time.YearMonth
@@ -32,7 +36,7 @@ import java.time.temporal.WeekFields
 import java.util.*
 
 @AndroidEntryPoint
-class CalendarFragment: BaseFragment<CalendarFragmentBinding>(R.layout.calendar_fragment) {
+class CalendarFragment : BaseFragment<CalendarFragmentBinding>(R.layout.calendar_fragment) {
 
     private lateinit var auth: FirebaseAuth
 
@@ -43,6 +47,7 @@ class CalendarFragment: BaseFragment<CalendarFragmentBinding>(R.layout.calendar_
 
         auth = Firebase.auth
 
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -52,28 +57,31 @@ class CalendarFragment: BaseFragment<CalendarFragmentBinding>(R.layout.calendar_
 
         setCalendarView()
 
-
-
-
     }
 
     class DayViewContainer(view: View) : ViewContainer(view) {
         var date = CalendarDayLayoutBinding.bind(view).calendarDayText
-        //var today = CalendarDayLayoutBinding.bind(view).ivTodayDot
     }
 
     private fun setCalendarView() {
-        with(binding.calendarView) {
+
+        binding.calendarView.apply {
+
             itemAnimator = null
+
             doOnPreDraw {
-                daySize = Size(binding.calendarView.width/7, binding.calendarView.height/5) // cell의 height
+                daySize = Size(
+                    binding.calendarView.width / 7,
+                    binding.calendarView.height / 5
+                ) // cell의 width, height
             }
 
             val currentMonth = YearMonth.now()
-            val firstMonth = currentMonth.minusMonths(240)
-            val lastMonth = currentMonth.plusMonths(240)
-            val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
-            setup(firstMonth, lastMonth, firstDayOfWeek)
+            val firstMonth = currentMonth.minusMonths(120)
+            val lastMonth = currentMonth.plusMonths(120)
+
+//            val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek // 이걸로 하면 일요일 시작임.
+            setup(firstMonth, lastMonth, DayOfWeek.MONDAY) // 여기에 넣으면 일요일 시작
             scrollToMonth(currentMonth)
 
             dayBinder = object : DayBinder<DayViewContainer> {
@@ -83,38 +91,156 @@ class CalendarFragment: BaseFragment<CalendarFragmentBinding>(R.layout.calendar_
                 }
 
                 override fun bind(container: DayViewContainer, day: CalendarDay) {
-                    val intent = Intent(requireContext(), com.myproject.dietproject.presentation.ui.MainActivity::class.java)
+
+                    val dayOfWeek = day.date.dayOfWeek
+
                     container.view.setOnClickListener {
-                        val clickedDate = "${day.date}" // df.format(day.date.monthValue) 인데 에러때문에 빼봄
-                        val action = CalendarFragmentDirections.actionCalendarFragmentToCalenderDetailFragment(auth.currentUser!!.uid, clickedDate)
-                        findNavController().navigate(action)
-                        Log.d("sdfsdf", clickedDate.toString())
+                        val clickedDate =
+                            "${day.date}" // df.format(day.date.monthValue) 인데 에러때문에 빼봄
+
+                        if (day.owner == DayOwner.THIS_MONTH) {
+                            val action =
+                                CalendarFragmentDirections.actionCalendarFragmentToCalenderDetailFragment(
+                                    auth.currentUser!!.uid,
+                                    clickedDate
+                                )
+                            findNavController().navigate(action)
+                        }
                     }
 
 
                     container.date.text = day.date.dayOfMonth.toString()
 
-                    if(day.owner  != DayOwner.THIS_MONTH) {
+                    /*                    container.date.isVisible = day.owner == DayOwner.THIS_MONTH
 
-                        container.date.setTextColor(ContextCompat.getColor(context, R.color.amber_500))
+                                        when(day.date) {
+                                            LocalDate.now() -> {
+                                                container.date.typeface = resources.getFont(R.font.gmarket_ttf_medium)
+                                                container.date.setTextColor(Color.BLACK)
+                                            }
+                                        }*/
 
-                        if(day.date.isAfter(LocalDate.now())) {
-                            container.date.setTextColor(ContextCompat.getColor(context, R.color.indigo_500))
-                        }
+                    if (day.owner != DayOwner.THIS_MONTH) { // 해당 달이 아닌 날짜
 
-                    } else {
+                        if (dayOfWeek == DayOfWeek.SUNDAY) { // 일요일
 
-                        when(day.date) {
-                            LocalDate.now() -> {
+                            container.date.typeface = resources.getFont(R.font.kangwon_light)
 
-                                container.date.typeface = resources.getFont(R.font.gmarket_ttf_medium)
-                                container.date.setTextColor(Color.BLACK)
-                                //container.today.visibility = View.VISIBLE
-                            }
+                            container.date.setTextColor(
+                                ContextCompat.getColor(
+                                    context,
+                                    R.color.invisible20red
+                                )
+                            )
+
+                        } else if (dayOfWeek == DayOfWeek.SATURDAY) { // 토요일
+
+                            container.date.typeface = resources.getFont(R.font.kangwon_light)
+
+                            container.date.setTextColor(
+                                ContextCompat.getColor(
+                                    context,
+                                    R.color.invisible20blue
+                                )
+                            )
+
+                        } else { // 월 화 수 목 금요일
+
+                            container.date.typeface = resources.getFont(R.font.kangwon_light)
+
+                            container.date.setTextColor(
+                                ContextCompat.getColor(
+                                    context,
+                                    R.color.invisible20
+                                )
+                            )
+
+
+                            /*                            if (day.date.isAfter(LocalDate.now())) {
+
+                                                            container.date.setTextColor(
+                                                                ContextCompat.getColor(
+                                                                    context,
+                                                                    R.color.invisible20
+                                                                )
+                                                            )
+                                                        }*/
                         }
                     }
+
+                    if (day.owner == DayOwner.THIS_MONTH) { // 해당 달에 포함 되는 날짜
+
+                        if (dayOfWeek == DayOfWeek.SUNDAY) { // 일요일
+
+                            container.date.setTextColor(
+                                ContextCompat.getColor(
+                                    context,
+                                    R.color.red_700
+                                )
+                            )
+
+                        } else if (dayOfWeek == DayOfWeek.SATURDAY) { // 토요일
+
+                            container.date.setTextColor(
+                                ContextCompat.getColor(
+                                    context,
+                                    R.color.blue_700
+                                )
+                            )
+                        } else {
+
+                            container.date.setTextColor(
+                                ContextCompat.getColor(
+                                    context,
+                                    R.color.black
+                                )
+                            )
+
+                        }
+                        if (day.date == LocalDate.now()) { // 현재 날짜
+                            container.date.typeface =
+                                resources.getFont(R.font.kangwon_light)
+
+                            container.date.setTextColor(
+                                ContextCompat.getColor(
+                                    context,
+                                    R.color.white
+                                )
+                            )
+
+                            container.date.background = ContextCompat.getDrawable(
+                                context,
+                                R.drawable.calendar_day_background_today
+                            )
+
+                            //container.today.visibility = View.VISIBLE
+                        } else {
+                            container.date.typeface = resources.getFont(R.font.kangwon_light)
+                            container.date.background = ContextCompat.getDrawable(
+                                context,
+                                R.drawable.calendar_day_background
+                            )
+                        }
+                    }
+
+
                 }
+
             }
+
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+
+                        binding.tvCalendarIndicator.text =
+                            findFirstVisibleMonth()?.yearMonth.toString() // 슬라이드로 넘겼을 때 해당 달의 텍스트 변경
+
+                    }
+                }
+            })
+
 
             binding.btnMonthNext.setOnClickListener {
                 binding.calendarView.findFirstVisibleMonth()?.let {
@@ -123,8 +249,6 @@ class CalendarFragment: BaseFragment<CalendarFragmentBinding>(R.layout.calendar_
                 }
             }
 
-
-
             binding.btnMonthPrev.setOnClickListener {
                 binding.calendarView.findFirstVisibleMonth()?.let {
                     binding.calendarView.smoothScrollToMonth(it.yearMonth.previous)
@@ -132,10 +256,13 @@ class CalendarFragment: BaseFragment<CalendarFragmentBinding>(R.layout.calendar_
                 }
             }
 
-            binding.tvCalendarIndicator.text = currentMonth.toString() // 해당 년-월 (2023-05) 달력 위에 띄워주는 부분인데 이거 LiveData로 바꿔서 ViewModel에서 계속 갱신하면서 받아오도록하면 될듯?
+            binding.tvCalendarIndicator.text =
+                currentMonth.toString() // 해당 년-월 (2023-05) 달력 위에 띄워주는 부분인데 이거 LiveData로 바꿔서 ViewModel에서 계속 갱신하면서 받아오도록하면 될듯?
             // 이거 지금 못하는 이유가 이걸 저기 button에 넣어버리면 계속 뷰가 갱신되는데 그거보단 그냥 viewModel에서 데이터 변경해서 받아와서 LiveData로 바꿔주는게 맞다 이건.
         }
+
     }
+
 
 //    private fun onMonthScroll(currentMonth: YearMonth) {
 //        val visibleMonth = binding.calendarView.findFirstVisibleMonth() ?: return
