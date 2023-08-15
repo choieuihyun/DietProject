b2f5dcd6c8adba794b944ddb95a5c609d08227d3
choeuihyeon <amammmaa1@naver.com>
2023-08-15 21:46:47 +0900

    feature : kakaotalk 공유 기능 버튼 구현

--- presentation/src/main/java/com/myproject/dietproject/presentation/ui/calendar/CalenderDetailFragment.kt
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/calendar/CalenderDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.myproject.dietproject.presentation.ui.calendar
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,13 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.kakao.sdk.common.util.KakaoCustomTabsClient
+import com.kakao.sdk.share.ShareClient
+import com.kakao.sdk.share.WebSharerClient
+import com.kakao.sdk.template.model.Button
+import com.kakao.sdk.template.model.Content
+import com.kakao.sdk.template.model.FeedTemplate
+import com.kakao.sdk.template.model.Link
 import com.myproject.dietproject.presentation.R
 import com.myproject.dietproject.presentation.databinding.CalendarDetailFragmentBinding
 import com.myproject.dietproject.presentation.ui.BaseFragment
@@ -56,8 +64,7 @@ class CalenderDetailFragment : BaseFragment<CalendarDetailFragmentBinding>(R.lay
             imageViewSetting()
         }
 
-
-
+        kakaoTest()
     }
 
     override fun onPause() {
@@ -88,6 +95,81 @@ class CalenderDetailFragment : BaseFragment<CalendarDetailFragmentBinding>(R.lay
             binding.calendarDetailImageView.setImageResource(R.drawable.calendar_detail_good)
 
     }
+
+    private fun kakaoTest() {
+
+        val defaultFeed = FeedTemplate(
+            content = Content(
+                title = "타이틀",
+                description = "설명~~~~~~~~",
+                imageUrl = "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+                link = Link(
+                    webUrl = "https://developers.kakao.com",
+                    mobileWebUrl = "https://developers.kakao.com"
+                )
+            ),
+            buttons = listOf(
+                Button(
+                    "웹으로 보기",
+                    Link(
+                        webUrl = "https://developers.kakao.com",
+                        mobileWebUrl = "https://developers.kakao.com"
+                    )
+                ),
+                Button(
+                    "앱으로 보기",
+                    Link(
+                        androidExecutionParams = mapOf("key1" to "value1", "key2" to "value2"),
+                        iosExecutionParams = mapOf("key1" to "value1", "key2" to "value2")
+                    )
+                )
+            )
+        )
+
+        binding.kakaoTalkButton.setOnClickListener {
+            // 카카오톡 설치여부 확인
+            if (ShareClient.instance.isKakaoTalkSharingAvailable(requireContext())) {
+                // 카카오톡으로 카카오톡 공유 가능
+                ShareClient.instance.shareDefault(requireContext(), defaultFeed) { sharingResult, error ->
+                    if (error != null) {
+                        Log.e("sdfsdfsdf", "카카오톡 공유 실패", error)
+                    }
+                    else if (sharingResult != null) {
+                        Log.d("sdfsdfsdf", "카카오톡 공유 성공 ${sharingResult.intent}")
+                        startActivity(sharingResult.intent)
+
+                        // 카카오톡 공유에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
+                        Log.w("sdfsdfsdf", "Warning Msg: ${sharingResult.warningMsg}")
+                        Log.w("sdfsdfsdf", "Argument Msg: ${sharingResult.argumentMsg}")
+                    }
+                }
+            } else {
+                // 카카오톡 미설치: 웹 공유 사용 권장
+                // 웹 공유 예시 코드
+                val sharerUrl = WebSharerClient.instance.makeDefaultUrl(defaultFeed)
+
+                // CustomTabs으로 웹 브라우저 열기
+
+                // 1. CustomTabsServiceConnection 지원 브라우저 열기
+                // ex) Chrome, 삼성 인터넷, FireFox, 웨일 등
+                try {
+                    KakaoCustomTabsClient.openWithDefault(requireContext(), sharerUrl)
+                } catch(e: UnsupportedOperationException) {
+                    // CustomTabsServiceConnection 지원 브라우저가 없을 때 예외처리
+                }
+
+                // 2. CustomTabsServiceConnection 미지원 브라우저 열기
+                // ex) 다음, 네이버 등
+                try {
+                    KakaoCustomTabsClient.open(requireContext(), sharerUrl)
+                } catch (e: ActivityNotFoundException) {
+                    // 디바이스에 설치된 인터넷 브라우저가 없을 때 예외처리
+                }
+            }
+        }
+    }
+
+
 
 
 }
