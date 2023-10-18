e56400db7b8af050b1af6cd9d102269aa141e33b
choeuihyeon <amammmaa1@naver.com>
2023-10-18 21:24:24 +0900

    feature : 잘못된 설계와 구현으로 인한 CalendarDetailViewModel 코드 재구현

--- presentation/src/main/java/com/myproject/dietproject/presentation/ui/calendar/CalendarDetailViewModel.kt
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/calendar/CalendarDetailViewModel.kt
@@ -1,34 +1,27 @@
 package com.myproject.dietproject.presentation.ui.calendar
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.myproject.dietproject.domain.model.TodayKcalForCalendar
+import com.myproject.dietproject.domain.model.KcalDataForCalendar
+import com.myproject.dietproject.domain.usecase.GetUserCalendarDetailUseCase
 import com.myproject.dietproject.domain.usecase.GetUserRecommendKcalUseCase
-import com.myproject.dietproject.domain.usecase.GetUserTodayKcalUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
-import kotlin.math.roundToInt
 
 @HiltViewModel
 class CalendarDetailViewModel @Inject constructor(
-    private val getUserTodayKcalUseCase: GetUserTodayKcalUseCase,
-    private val getUserRecommendKcalUseCase: GetUserRecommendKcalUseCase
+    private val getUserRecommendKcalUseCase: GetUserRecommendKcalUseCase,
+    private val getUserCalendarDetailUseCase: GetUserCalendarDetailUseCase
 ) : ViewModel() {
 
     private val _dayKcal: MutableLiveData<Int> = MutableLiveData()
     val dayKcal: LiveData<Int> = _dayKcal
 
-    private val _dayKcalList: MutableLiveData<ArrayList<TodayKcalForCalendar?>> = MutableLiveData()
-    val dayKcalList: LiveData<ArrayList<TodayKcalForCalendar?>> = _dayKcalList
+    private val _dayKcalList: MutableLiveData<ArrayList<KcalDataForCalendar?>> = MutableLiveData()
+    val dayKcalList: LiveData<ArrayList<KcalDataForCalendar?>> = _dayKcalList
 
     private var _recommendKcal: MutableLiveData<Int> = MutableLiveData()
     val recommendKcal: LiveData<Int> = _recommendKcal
@@ -41,63 +34,27 @@ class CalendarDetailViewModel @Inject constructor(
 
     fun getCalendarDetailData(userId: String, date: String) {
 
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch {
 
-            var sum = 0
-            val kcalArrayList = ArrayList<TodayKcalForCalendar?>()
+            getUserCalendarDetailUseCase(userId, date)
 
-            getUserTodayKcalUseCase(userId, date).addListenerForSingleValueEvent(object :
-                ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
+            _dayKcal.value = getUserCalendarDetailUseCase.getDayKcal()
 
-                    for (data in snapshot.children) {
-                        val dateData = data.key?.substring(0, 10)
+            _dayKcalList.value = getUserCalendarDetailUseCase.getDayKcalList()
 
-                        if (dateData == date) {
-                            val kcal = data.child("kcal").value
-                            sum += kcal.toString().toInt()
-                            val todayKcalForCalendar =
-                                TodayKcalForCalendar( // 이 구조가 아닌거 같음. 그냥 받아오는 데이터로 해야지 이건 아니다.
-                                    kcal = kcal.toString().toFloat().roundToInt(),
-                                    foodName = data.child("foodName").value.toString(),
-                                    makerName = data.child("makerName").value.toString()
-                                )
-                            kcalArrayList.add(todayKcalForCalendar)
-                        }
-
-                    }
-
-                    _dayKcalList.value = kcalArrayList // 어차피 읽어오기만 하니까 읽기 전용 리스트로의 변환인 listOf
-                    _dayKcal.value = sum
-
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-            })
         }
 
     }
 
     fun getRecommendKcalData(userId: String) {
 
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch {
 
-            getUserRecommendKcalUseCase(userId).addListenerForSingleValueEvent(object :
-                ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
+            getUserRecommendKcalUseCase(userId)
 
-                    _recommendKcal.value = snapshot.value.toString().toInt()
-                    imageSetting()
+            _recommendKcal.value = getUserRecommendKcalUseCase.getRecommendKcal().value
 
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-
-            })
+            imageSetting()
 
         }
 
