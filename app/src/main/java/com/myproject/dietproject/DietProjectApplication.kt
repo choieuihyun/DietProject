f5641923c9a5e8eb8229f4effb800824ac793f82
choeuihyeon <amammmaa1@naver.com>
2023-11-13 21:25:53 +0900

    feature : 앱 실행시 브로드캐스트 정보 받아오기

--- app/src/main/java/com/myproject/dietproject/DietProjectApplication.kt
+++ app/src/main/java/com/myproject/dietproject/DietProjectApplication.kt
@@ -1,12 +1,38 @@
 package com.myproject.dietproject
 
 import android.app.Application
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class DietProjectApplication : Application() {
 
+    private lateinit var mReceiver: BatteryReceiver
+
     override fun onCreate() {
         super.onCreate()
+
+        mReceiver = BatteryReceiver()
+
+        val intentFilter = IntentFilter()
+
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED)
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
+        intentFilter.addAction(mReceiver.batteryAction)
+        registerReceiver(mReceiver, intentFilter)
+
+        val intent = Intent(mReceiver.batteryAction)
+        sendBroadcast(intent)
     }
+
+    override fun onTerminate() {
+        super.onTerminate()
+
+        unregisterReceiver(mReceiver)
+    }
+
+
 }
