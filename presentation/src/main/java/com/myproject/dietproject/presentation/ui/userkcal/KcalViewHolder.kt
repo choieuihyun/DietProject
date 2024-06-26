39e749f575f2744508c0e10010bd152ed81fd1f0
choieuihyun <amammmaa1@naver.com>
2024-06-26 22:23:26 +0900

    feature: 즐겨찾기 버튼 구현에 따른 뷰홀더 구현

--- presentation/src/main/java/com/myproject/dietproject/presentation/ui/userkcal/KcalViewHolder.kt
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/userkcal/KcalViewHolder.kt
@@ -1,17 +1,28 @@
 package com.myproject.dietproject.presentation.ui.userkcal
 
 import android.util.Log
+import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
 import com.myproject.dietproject.domain.model.Kcal
+import com.myproject.dietproject.presentation.R
 import com.myproject.dietproject.presentation.databinding.KcalListItemBinding
 
 class KcalViewHolder(
     private val binding: KcalListItemBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
+    val imageButton = binding.favoriteButton
+
     fun bind(userKcal: Kcal) {
 
         binding.kcalData = userKcal
+        if(userKcal.favoriteButtonState) {
+            imageButton.isSelected = true
+            imageButton.setBackgroundResource(R.drawable.star_filled)
+        } else {
+            imageButton.isSelected = false
+            imageButton.setBackgroundResource(R.drawable.star_border)
+        }
 
     }
 
--- presentation/src/main/java/com/myproject/dietproject/presentation/ui/userkcal/KcalViewHolderTypeTwo.kt
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/userkcal/KcalViewHolderTypeTwo.kt
@@ -1,17 +1,25 @@
 package com.myproject.dietproject.presentation.ui.userkcal
 
-import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import com.myproject.dietproject.domain.model.Kcal
+import com.myproject.dietproject.presentation.R
 import com.myproject.dietproject.presentation.databinding.KcalListItemTypeTwoBinding
 
 class KcalViewHolderTypeTwo(
     private val binding: KcalListItemTypeTwoBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
+    val imageButton = binding.favoriteButton
     fun bind(userKcal: Kcal) {
 
         binding.kcalData = userKcal
+        if(userKcal.favoriteButtonState) {
+            imageButton.isSelected = true
+            imageButton.setBackgroundResource(R.drawable.star_filled)
+        } else {
+            imageButton.isSelected = false
+            imageButton.setBackgroundResource(R.drawable.star_border)
+        }
 
     }
 
