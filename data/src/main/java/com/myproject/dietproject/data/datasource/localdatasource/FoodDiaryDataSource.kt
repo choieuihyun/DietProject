d4acd3d37647bc4977d67c042dc102e7aa9a4b42
choieuihyun <amammmaa1@naver.com>
2024-07-04 23:34:03 +0900

    refactor: data layer 실시간 데이터 표시를 위한 LiveData -> Flow 변경, Flow 도입

--- data/src/main/java/com/myproject/dietproject/data/datasource/localdatasource/FoodDiaryDataSource.kt
+++ data/src/main/java/com/myproject/dietproject/data/datasource/localdatasource/FoodDiaryDataSource.kt
@@ -1,14 +1,18 @@
 package com.myproject.dietproject.data.datasource.localdatasource
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import com.myproject.dietproject.data.db.local.db.FoodDiaryDatabase
 import com.myproject.dietproject.data.db.local.entity.FoodDiaryEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
 
 class FoodDiaryDataSource @Inject constructor(
     private val database: FoodDiaryDatabase
 ){
 
-    fun getAllFoodDiary(): List<FoodDiaryEntity> {
+    fun getAllFoodDiary(): Flow<List<FoodDiaryEntity>?> {
         return database.kcalDao().getAllKcal()
     }
 
@@ -16,8 +20,8 @@ class FoodDiaryDataSource @Inject constructor(
         database.kcalDao().insertKcal(foodDiary)
     }
 
-    suspend fun deleteFoodDiary(foodDiary: FoodDiaryEntity) {
-        database.kcalDao().deleteKcal(foodDiary)
+    suspend fun deleteFoodDiary(foodName: String) {
+        database.kcalDao().deleteKcal(foodName)
     }
 
 
--- data/src/main/java/com/myproject/dietproject/data/db/local/dao/FoodDiaryDao.kt
+++ data/src/main/java/com/myproject/dietproject/data/db/local/dao/FoodDiaryDao.kt
@@ -1,22 +1,29 @@
 package com.myproject.dietproject.data.db.local.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.myproject.dietproject.data.db.local.entity.FoodDiaryEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FoodDiaryDao {
 
+//    @Query("SELECT * from foodDiary")
+//    // Delete시에 UI의 즉각적인 변경을 위한 LiveData 반환을 하려했는데 Dao가 LiveData 반환하면 다 Null이 뜨는데?
+//    fun getAllKcal(): List<FoodDiaryEntity>
+
     @Query("SELECT * from foodDiary")
-    fun getAllKcal(): List<FoodDiaryEntity>
+    // Delete시에 UI의 즉각적인 변경을 위한 LiveData 반환을 하려했는데 Dao가 LiveData 반환하면 다 Null이 뜨는데?
+    fun getAllKcal(): Flow<List<FoodDiaryEntity>>
 
     // 충돌 무시하는 파라미터가 있는데 내 앱의 구조 상 충돌이 일어날 것 같진 않다.
     @Insert
     suspend fun insertKcal(foodDiary: FoodDiaryEntity)
 
-    @Delete
-    suspend fun deleteKcal(foodDiary: FoodDiaryEntity)
+    @Query("DELETE FROM foodDiary WHERE foodName = :foodName")
+    suspend fun deleteKcal(foodName: String)
 
 }
