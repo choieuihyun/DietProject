dc8c671580cfcb86f05177262a15af0909597e5d
choieuihyun <amammmaa1@naver.com>
2024-07-05 23:41:10 +0900

    feature: 검색 리스트 화면, 즐겨찾기 리스트 화면 프래그먼트 구현

---
+++ presentation/src/main/java/com/myproject/dietproject/presentation/ui/userkcal/KcalFragmentList.kt
@@ -0,0 +1,149 @@
+package com.myproject.dietproject.presentation.ui.userkcal
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.myproject.dietproject.domain.model.FoodDiaryModel
+import com.myproject.dietproject.domain.model.Kcal
+import com.myproject.dietproject.presentation.R
+import com.myproject.dietproject.presentation.databinding.KcalFragmentListBinding
+import com.myproject.dietproject.presentation.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class KcalFragmentList : BaseFragment<KcalFragmentListBinding>(R.layout.kcal_fragment_list) {
+
+    private lateinit var recyclerViewAdapter: KcalFragmentListAdapter
+    private val viewModel: KcalViewModel by activityViewModels()
+    private lateinit var userId: String
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d("onAttachKcalList", "onAttachKcalList")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("onCreateKcalList", "onCreateKcalList")
+
+        userId = viewModel.userId
+        viewModel.setSharedPreference("favoriteButtonPreference")
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        Log.d("onCreateViewKcalList", "onCreateViewKcalList")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d("onViewCreatedKcalList", "onViewCreatedKcalList")
+        setupRecyclerView()
+
+        viewModel.getEmptyKcalData()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("onStartKcalList", "onStartKcalList")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        viewModel.updateKcalListFavoriteState()
+
+        viewModel.kcalData.observe(viewLifecycleOwner) {
+
+            if (it != null) {
+                recyclerViewAdapter.submitList(it)
+                for(i in it) {
+                    if(i.favoriteButtonState)
+                        Log.d("kcal123", i.dESCKOR.toString())
+                }
+            } else
+                binding.kcalRecyclerView.visibility = View.GONE
+
+        }
+
+        recyclerViewAdapter.setOnItemClickListener { kcal ->
+            val action = KcalFragmentDirections.actionKcalFragmentToKcalDetailFragment(userId,null , kcal)
+            findNavController().navigate(action)
+        }
+
+        recyclerViewAdapter.setOnFavoriteButtonClickListener { kcal, imageButton ->
+
+            val foodName = kcal.dESCKOR
+
+            if(imageButton.isSelected) {
+                // 개인적으로 이런 구조도 view에 Model에 관한 코드가 있어서 올바른건가 싶긴 하다.
+                val foodDiary = FoodDiaryModel(
+                    kcal = kcal.nUTRCONT1?.toFloat(),
+                    foodName = kcal.dESCKOR,
+                    makerName = kcal.mAKERNAME,
+                    favoriteButtonState = true
+                )
+                viewModel.addFoodDiary(foodDiary)
+                viewModel.addSharedPreferenceFavoriteState(foodName!!, foodName)
+            }
+            else {
+
+                // 이것도 이름으로 해도 될거같은데?
+                val foodDiary = FoodDiaryModel(
+                    kcal = kcal.nUTRCONT1?.toFloat(),
+                    foodName = kcal.dESCKOR,
+                    makerName = kcal.mAKERNAME,
+                    favoriteButtonState = false
+                )
+                viewModel.deleteSharedPreferenceFavoriteState(foodName!!)
+                viewModel.deleteFoodDiary(foodDiary.foodName!!)
+                //viewModel.resetFavoriteList()
+
+            }
+
+        }
+
+        Log.d("onResumeKcalList", "onResumeKcalList")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("onPauseKcalList", "onPauseKcalList")
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("onDestroyViewKcalList", "onDestroyViewKcalList")
+        //recyclerViewAdapter.submitList(emptyList())
+    }
+
+    private fun setupRecyclerView() {
+
+        recyclerViewAdapter = KcalFragmentListAdapter()
+
+        binding.kcalRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = recyclerViewAdapter
+        }
+
+    }
+
+
+}
