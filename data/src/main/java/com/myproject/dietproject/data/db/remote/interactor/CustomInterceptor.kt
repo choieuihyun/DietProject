7e7dd216bb9006d4b530de0d017ae10c3e7a607a
choieuihyun <amammmaa1@naver.com>
2024-08-15 14:16:46 +0900

    refactor: Interceptor Log 추가

--- data/src/main/java/com/myproject/dietproject/data/db/remote/interactor/CustomInterceptor.kt
+++ data/src/main/java/com/myproject/dietproject/data/db/remote/interactor/CustomInterceptor.kt
@@ -1,10 +1,12 @@
-package com.myproject.dietproject.data.di
+package com.myproject.dietproject.data.db.remote.interactor
 
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
 class CustomInterceptor : Interceptor {
+    // 쿼리 파라미터와 경로 변수의 차이 때문에 이렇게 된거
+    // 서버에서 제공하는 규칙에 따라가야해서 만든 인터셉터
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request: Request = chain.request()
