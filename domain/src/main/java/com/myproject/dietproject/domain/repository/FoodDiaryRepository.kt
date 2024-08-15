fbadac4aab55eb82271d78a4d5da4ffd369cc7fe
choieuihyun <amammmaa1@naver.com>
2024-08-15 14:20:15 +0900

    refactor: 즐겨찾기 데이터 Flow 적용

--- domain/src/main/java/com/myproject/dietproject/domain/repository/FoodDiaryRepository.kt
+++ domain/src/main/java/com/myproject/dietproject/domain/repository/FoodDiaryRepository.kt
@@ -1,13 +1,14 @@
 package com.myproject.dietproject.domain.repository
 
 import com.myproject.dietproject.domain.model.FoodDiaryModel
+import kotlinx.coroutines.flow.Flow
 
 interface FoodDiaryRepository {
 
-    fun getAllFood(): List<FoodDiaryModel>
+    fun getAllFood(): Flow<List<FoodDiaryModel>?>
 
     suspend fun insertFood(foodDiaryModel: FoodDiaryModel)
 
-    suspend fun deleteFood(foodDiaryModel: FoodDiaryModel)
+    suspend fun deleteFood(foodName: String)
 
 }
