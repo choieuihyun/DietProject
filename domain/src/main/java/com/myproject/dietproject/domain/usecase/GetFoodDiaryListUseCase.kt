1db33bdfe449e32e4f3c4b81a41e68317df5edab
choieuihyun <amammmaa1@naver.com>
2024-08-15 14:20:26 +0900

    refactor: 즐겨찾기 데이터 Flow 적용

--- domain/src/main/java/com/myproject/dietproject/domain/usecase/GetFoodDiaryListUseCase.kt
+++ domain/src/main/java/com/myproject/dietproject/domain/usecase/GetFoodDiaryListUseCase.kt
@@ -2,11 +2,12 @@ package com.myproject.dietproject.domain.usecase
 
 import com.myproject.dietproject.domain.model.FoodDiaryModel
 import com.myproject.dietproject.domain.repository.FoodDiaryRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetFoodDiaryListUseCase @Inject constructor(private val repository: FoodDiaryRepository) {
 
-    operator fun invoke(): List<FoodDiaryModel> {
+    operator fun invoke(): Flow<List<FoodDiaryModel>?> {
         return repository.getAllFood()
     }
 
